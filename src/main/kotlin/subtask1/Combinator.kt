cadff9aa31f4a39d7fc2277604fa7619f10480ba
maksim_y <maksimyarmolich@mail.ru>
2021-07-12 19:42:24 +0300

    completed 1,3 tasks

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,22 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val posters = array[0].toLong()
+        val colors  = array[1].toLong()
+        var k = 0L
+        var calcPosters = 0L
+
+        while (calcPosters < posters) {
+            if ( ++k == colors )
+                return null
+            calcPosters = factorial(colors) / (factorial(k) * (factorial(colors - k)))
+        }
+        return k.toInt()
+    }
+    private fun factorial(num: Long): Long {
+        var result = 1L
+        for (i in 2..num) result *= i
+        return result
     }
 }
+
