87ee4947da848dd6b4db75dc780b7f6bffdeee21
maksim_y <maksimyarmolich@mail.ru>
2021-07-14 21:39:44 +0300

    add: second task completed

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,27 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return if (number < 3) return null
+        else calculate(number.pow(), number - 1)?.toTypedArray()
     }
+    private fun calculate (number: Int, index: Int): ArrayList<Int>?{
+        for(i in index downTo 1){
+            val remained = number - i.pow()
+            if(remained == 0) return arrayListOf(i)
+            if(remained < 0) return null
+            var element = remained.sqrt()
+            if(element >= i) element = i-1
+            val result = calculate(remained, element)
+            if(result !=null){
+                result.add(i)
+                return result
+            }
+        }
+        return null
+    }
+    private fun Int.pow() = this * this
+    private fun Int.sqrt() = kotlin.math.sqrt(this.toDouble()).toInt()
 }
+
+
